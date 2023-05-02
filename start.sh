2d7c24c25e6b7fad98fce9f2d7e0b1367c8e61d8
FGNF <132178606+FGNF@users.noreply.github.com>
2023-05-02 08:22:22 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,20 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=998013e0-1ac9-417f-8abe-72d384bfd343
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=gameshedge.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
